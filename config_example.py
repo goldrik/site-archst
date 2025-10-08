4ad0b1cbaf29d35ce7da0eea9582ccb03ce85b68
Aurik Sarker <auriksarker@gmail.com>
2025-10-08 17:28:03 -0400

    Move protected variables to config.py. Push only the example config file, update .gitignore accordingly

---
+++ config_example.py
@@ -0,0 +1,10 @@
+# Copy this file to config.py
+#   then fill in the variables
+
+# Path to save images
+DIR_UPLOADS_SLIDESHOW=
+DIR_UPLOADS_WEBCAM=
+
+
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///site.sqlite'
