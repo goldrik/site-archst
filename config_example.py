0d251d3b23db8f4d50cd6a0bfdd8b82b0dca0c2f
Aurik Sarker <auriksarker@gmail.com>
2025-10-18 03:59:04 -0400

    Sectioned config file

--- config_example.py
+++ config_example.py
@@ -1,10 +1,14 @@
 # Copy this file to config.py
 #   then fill in the variables
 
-# Path to save images
-DIR_UPLOADS_SLIDESHOW=
-DIR_UPLOADS_WEBCAM=
 
+# SLIDESHOW
+DIR_UPLOADS_SLIDESHOW=''
+
+# WEBCAM
+DIR_UPLOADS_WEBCAM=''
+
+# LIGHT
 
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.sqlite'
